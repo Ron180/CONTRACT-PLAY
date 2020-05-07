8867efa08d6b8990dc131274e99c157e7889f2a8
Ron180 <64553203+Ron180@users.noreply.github.com>
2020-05-07 15:22:19 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.contractplayfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent("com.example.Registrate1"));
+    }
+    public void onClick2(View view) {
+        startActivity(new Intent("com.example.MENU"));
+    }
+
+
+}
