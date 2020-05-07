f3f42566bb0ff40244553c629e06b24914908679
Ron180 <64553203+Ron180@users.noreply.github.com>
2020-05-07 15:21:55 -0500

    Add files via upload

---
+++ ContratoGeneral.java
@@ -0,0 +1,12 @@
+package com.example.contractplayfinal;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContratoGeneral extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contrato);
+    }
+}
