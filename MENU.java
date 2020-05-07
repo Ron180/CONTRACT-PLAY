3aa367d14572b11bf3e1e042ec057ad20e42bb3f
Ron180 <64553203+Ron180@users.noreply.github.com>
2020-05-07 15:23:04 -0500

    Add files via upload

---
+++ MENU.java
@@ -0,0 +1,21 @@
+package com.example.contractplayfinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MENU extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+    public void onClick3(View view){
+        startActivity(new Intent("com.example.CONTRATOGENERAL"));
+    }
+    public void onClick4(View view){
+        startActivity(new Intent("com.example.BUFFETDEABOGADOS"));
+    }
+}
