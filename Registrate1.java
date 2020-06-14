998c0f3246864422b464f4e80f9faaeb729d641f
Ronald Machaca Hilasaca <64553203+RonaldMachacaHilasaca@users.noreply.github.com>
2020-06-14 10:49:21 -0500

    Update Registrate1.java

--- Registrate1.java
+++ Registrate1.java
@@ -1,18 +1,60 @@
 package com.example.contractplayfinal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 public class Registrate1 extends AppCompatActivity {
+
+    private Button mUploadBtn;
+    private StorageReference mStorage;
+    private static final int GALLERY_INTENT=1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrate1);
+        setContentView(R.layout.activity_registrate18);
     }
-    public void registroExistoso(View view){
-        Toast.makeText(this, "Se ah regristrado \n correctamente", Toast.LENGTH_LONG).show();
+
+    public void RegistroImagen (View view){
+    mUploadBtn = (Button)findViewById(R.id.btn_subirImagen);
+
+        mUploadBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent=new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent,GALLERY_INTENT);
+
+        }
+    });
+}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+
+            StorageReference fiePath=mStorage.child("fotos").child(uri.getLastPathSegment());
+            fiePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(Registrate1.this,"Se subio la imagen",Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
     }
 }
