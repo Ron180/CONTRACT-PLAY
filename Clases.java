2791c78240f9ee77b99cb7709aa7d7a967fe983d
Ronald Machaca Hilasaca <64553203+RonaldMachacaHilasaca@users.noreply.github.com>
2020-06-14 10:55:27 -0500

    Add files via upload

---
+++ Clases.java
@@ -0,0 +1,23 @@
+package com.example.contractplayfinal.db_firebase;
+
+public class Clases {
+    String claseid, seccion, area, tema;
+    public Clases(String claseid, String seccion, String area, String tema){
+        this.claseid = claseid;
+        this.seccion = seccion;
+        this.area = area;
+        this.tema = tema;
+    }
+    public String getClaseid() {
+        return claseid;
+    }
+    public String getSeccion() {
+        return seccion;
+    }
+    public String getArea() {
+        return area;
+    }
+    public String getTema() {
+        return tema;
+    }
+}
