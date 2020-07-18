6969b23a46adfff605073b9a5e2d937c80173c51
Ronald Machaca Hilasaca <64553203+RonaldMachacaHilasaca@users.noreply.github.com>
2020-07-18 14:20:53 -0500

    Update MiFirebaseInstanceIdService.java

--- MiFirebaseInstanceIdService.java
+++ MiFirebaseInstanceIdService.java
@@ -2,7 +2,7 @@ package com.example.contractplayfinal;
         import android.util.Log;
         import com.google.firebase.iid.FirebaseInstanceId;
         import com.google.firebase.iid.FirebaseInstanceIdService;
-
+// desarrollado por Ronald Machaca .
 public class MiFirebaseInstanceIdService extends FirebaseInstanceIdService {
     String TAG ="NOTICIAS";
     @Override
