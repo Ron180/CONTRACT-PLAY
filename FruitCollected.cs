af9d5c0f7e5b1db503d77aa7f22f8e6281ca12ba
Ronald Machaca Hilasaca <64553203+RonaldMachacaHilasaca@users.noreply.github.com>
2020-07-18 14:12:47 -0500

    Add files via upload

---
+++ FruitCollected.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitCollected : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    //colicion entre las frutas y el player 
+    {
+        if (collision.CompareTag("Player"))
+        //para que collisione con el "player" usamos un CompareTag 
+
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            // Desactivamos el sprite renderer; para que la fruta desaparesca una vez colisione.
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            // para activar la animacion el objeto collected  
+            Destroy(gameObject, 0.5f);
+            //  para eliminar la fruta; una vez colisionemos usamos un destroy en un tiempo de 0.5 segundos.
+        }
+    }
+}
