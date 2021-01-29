a61de50501406db24f019a712e8702b233d1f556
Ronald Machaca Hilasaca <64553203+RonaldMachacaHilasaca@users.noreply.github.com>
2021-01-28 22:26:32 -0500

    Update ContratoGeneral.java

--- ContratoGeneral.java
+++ ContratoGeneral.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-// desarrollado por dudykoft idme calderon.
+// desarrollado por Ronald Machaca Hilasaca.
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.contractplayfinal.db_firebase.Clases;
