41a5ee6a423a471495ae183600a35e29cf9ec48b
Ronald Machaca Hilasaca <64553203+RonaldMachacaHilasaca@users.noreply.github.com>
2021-01-28 22:28:56 -0500

    Update MyService.java

--- MyService.java
+++ MyService.java
@@ -9,7 +9,7 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 
-
+//
 public class MyService extends FirebaseMessagingService {
 
     private static final String TAG ="0";
